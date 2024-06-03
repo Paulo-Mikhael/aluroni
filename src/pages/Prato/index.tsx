95fc4d15700f503d73d3172ee658a4fc2b48b0ca
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-06-03 09:31:18 -0400

    Projeto final

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,36 +1,44 @@
 import classNames from 'classnames';
 import styles from './Prato.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import Cardapio from 'data/cardapio.json';
 import TagsPrato from 'components/TagsPrato';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 
-export default function Prato(){
+export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = Cardapio.find(item => item.id === Number(id));
-  if (!prato){
-    return '';
+  if (!prato) {
+    return <NotFound />;
   }
 
   return (
-    <>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>
-          {prato.title}
-        </h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__description}>
-            {prato.description}
-          </p>
-        </div>
-        <TagsPrato {...prato} />
-      </section>
-    </>
+    <Routes>
+      <Route path='*' element={<PaginaPadrao />}>
+        <Route index element={
+          <>
+            <button className={styles.voltar} onClick={() => navigate(-1)}>
+              {'< Voltar'}
+            </button>
+            <section className={styles.container}>
+              <h1 className={styles.titulo}>
+                {prato.title}
+              </h1>
+              <div className={styles.imagem}>
+                <img src={prato.photo} alt={prato.title} />
+              </div>
+              <div className={styles.conteudo}>
+                <p className={styles.conteudo__description}>
+                  {prato.description}
+                </p>
+              </div>
+              <TagsPrato {...prato} />
+            </section>
+          </>
+        } />
+      </Route>
+    </Routes>
   );
 }
