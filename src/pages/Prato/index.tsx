5e029d38636d45e9491d61a4b45ae9195e8e76f1
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-07-04 11:34:33 -0400

    Otimizando performance - final

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,14 +1,17 @@
 import styles from './Prato.module.scss';
 import { useParams, useNavigate, Routes, Route } from 'react-router-dom';
 import Cardapio from 'data/cardapio.json';
-import TagsPrato from 'components/TagsPrato';
-import NotFound from 'pages/NotFound';
-import PaginaPadrao from 'components/PaginaPadrao';
+import { lazy } from 'react';
+
+const TagsPrato = lazy(() => import('components/TagsPrato'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+const NotFound = lazy(() => import('pages/NotFound'));
 
 export default function Prato() {
   const { id } = useParams();
   const navigate = useNavigate();
   const prato = Cardapio.find(item => item.id === Number(id));
+
   if (!prato) {
     return <NotFound />;
   }
